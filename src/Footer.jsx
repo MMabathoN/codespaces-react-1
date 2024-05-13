12ee1a340a16ac5bf0ac26e029429a16086caa30
MMabathoN <161308587+MMabathoN@users.noreply.github.com>
2024-05-13 11:41:11 +0000

    code almost compelete  i just need to fix  display issues

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p> a herf="https://refactored-cod-4jqqppgx7wrv3jq9r.github.dev/"</p>
+      <p>© 2024 Weather App</p>
+    </footer>
+  );
+};
+
+export default Footer;
